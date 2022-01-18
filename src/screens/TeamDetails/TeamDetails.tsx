b9c2a028be9676f311c302f91bf7d1e8d611d7c2
bartoszboruta <bboruta.dev@gmail.com>
2022-01-18 14:44:21 +0100

    feature/add top motivators data

--- src/screens/TeamDetails/TeamDetails.tsx
+++ src/screens/TeamDetails/TeamDetails.tsx
@@ -1,9 +1,52 @@
-import Layout from '../../components/Layout'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Spin, Space } from "antd";
+
+import { Card, Layout } from "../../components";
 
 const TeamDetails = () => {
+  const [loading, setLoading] = useState(true);
+  const [topMotivators, setTopMotivators] = useState([]);
+
+  useEffect(() => {
+    const getTopMotivators = async () => {
+      try {
+        const response = await axios.get(
+          "https://motivation-cards-backend.herokuapp.com/api/v1/admin/top_motivations",
+          {
+            headers: {
+              Authorization: `${localStorage.getItem("api_token")}`,
+            },
+          }
+        );
+        setTopMotivators(response.data);
+        setLoading(false);
+      } catch (e) {
+        setLoading(false);
+      }
+    };
+    getTopMotivators();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loader">
+        <Space size="middle">
+          <Spin size="large" />
+        </Space>
+      </div>
+    );
+  }
+
+  if (!topMotivators.length) {
+    return <div>Your team has no motivators</div>;
+  }
+
   return (
     <Layout>
-      <div>TeamDetails</div>
+      {topMotivators.map((motivator, index) => (
+        <Card key={index} card={motivator} />
+      ))}
     </Layout>
   );
 };
