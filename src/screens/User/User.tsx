22e8883f56103763249c0cedcc56cc0f228b0e9e
bartoszboruta <bboruta.dev@gmail.com>
2022-01-18 14:26:18 +0100

    chore/add workspace to layout

--- src/screens/User/User.tsx
+++ src/screens/User/User.tsx
@@ -1,57 +1,72 @@
-import { useEffect, useState } from 'react'
-import Layout from '../../components/Layout'
-import { Select, Button } from 'antd';
+import { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import { Select, Button } from "antd";
 import { useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 const User = () => {
-  const navigate = useNavigate()
-  const [teamList, setTeamList] = useState([])
-  const [selectedTeam, setSelectedTeam] = useState('')
+  const navigate = useNavigate();
+  const [teamList, setTeamList] = useState([]);
+  const [selectedTeam, setSelectedTeam] = useState("");
   useEffect(() => {
     const getUserInfo = async () => {
-      const response = await axios.get('https://motivation-cards-backend.herokuapp.com/api/v1/user/user_info', {
-        headers: {
-          'Authorization': `${localStorage.getItem('api_token')}`
+      const response = await axios.get(
+        "https://motivation-cards-backend.herokuapp.com/api/v1/user/user_info",
+        {
+          headers: {
+            Authorization: `${localStorage.getItem("api_token")}`,
+          },
         }
-      })
+      );
       if (response.data.team) {
-        navigate('user-motivators')
+        navigate("workspace");
       }
-    }
+    };
 
     const getTeams = async () => {
-      const response:any = await axios.get('https://motivation-cards-backend.herokuapp.com/api/v1/user/teams', {
-        headers: {
-          'Authorization': `${localStorage.getItem('api_token')}`
+      const response: any = await axios.get(
+        "https://motivation-cards-backend.herokuapp.com/api/v1/user/teams",
+        {
+          headers: {
+            Authorization: `${localStorage.getItem("api_token")}`,
+          },
         }
-      })
+      );
 
-      setTeamList(response.data)
-    }
+      setTeamList(response.data);
+    };
 
-    getUserInfo()
-    getTeams()
-  }, [])
+    getUserInfo();
+    getTeams();
+  }, []);
 
   const handleTeamSubmit = () => {
-    return axios.post('https://motivation-cards-backend.herokuapp.com/api/v1/user/teams', {
-      team_id: selectedTeam,
-    },
-    {
-      headers: {
-      'Authorization': `${localStorage.getItem('api_token')}`
-    }}).then(() => navigate('/user-motivators'))
-  }
+    return axios
+      .post(
+        "https://motivation-cards-backend.herokuapp.com/api/v1/user/teams",
+        {
+          team_id: selectedTeam,
+        },
+        {
+          headers: {
+            Authorization: `${localStorage.getItem("api_token")}`,
+          },
+        }
+      )
+      .then(() => navigate("/user-motivators"));
+  };
 
   const handleSelectChange = (event: string) => {
-    setSelectedTeam(event)
-  }
+    setSelectedTeam(event);
+  };
 
   return (
     <Layout>
       {teamList.length > 0 && (
-        <Select placeholder="Choose your team" onChange={(event) => handleSelectChange(event)}>
+        <Select
+          placeholder="Choose your team"
+          onChange={(event) => handleSelectChange(event)}
+        >
           {teamList.map(({ id, name }) => (
             <Select.Option value={id} key={id}>
               {name}
