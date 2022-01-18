1e49348616a876bdaea538a030c1b61332517148
Dawid Kawiak <d.kawiak@selleo.com>
2022-01-18 10:34:03 +0100

    Add basic routes

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,1 +1,6 @@
-export { default as Main } from "./Main";
+export { default as Login } from "./Login";
+export { default as User } from "./User";
+export { default as UserMotivators } from "./UserMotivators";
+export { default as Admin } from "./Admin";
+export { default as TeamDetails } from "./TeamDetails";
+export { default as AppRoutes } from "./AppRoutes";
