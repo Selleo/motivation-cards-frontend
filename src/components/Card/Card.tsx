9200d09141d3fd90b0288e2890ddba728e90db7e
bartoszboruta <bboruta.dev@gmail.com>
2022-01-18 12:14:58 +0100

    feature/add card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,7 +1,32 @@
-const Card = () => {
+import React from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+import { ICard } from "../../types/types";
+
+interface ICardProps {
+  card: ICard;
+}
+const Card: React.FC<ICardProps> = ({ card }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: card.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <div>
-      <div>Card</div>
+    <div
+      className="card"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      <img className="card__image" alt="card" src={card.imageUrl} />
+      <div className="card__title">{card.title}</div>
+      <div className="card__description">{card.description}</div>
     </div>
   );
 };
